9c7f4ffebba1cf21c8d87d4bf7ddf91e13420c72
ndqkhanh20 <khanhndq2002@gmail.com>
2024-04-24 00:05:15 +0700

    [BE] - feat: fix something

--- create-topics.sh
+++ create-topics.sh
@@ -1,14 +1,39 @@
 #gke-confluent-kafka-cp-zookeeper-headless
 kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic code-question-create-request-to-core-service --delete --if-exists
 kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic code-question-create-response-from-core-service --delete --if-exists
+
 kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-create-request --delete --if-exists
 kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-create-response --delete --if-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-update-request --delete --if-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-update-response --delete --if-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-delete-request --delete --if-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-delete-response --delete --if-exists
+
 kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic user-request --delete --if-exists
 kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic user-response --delete --if-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic user-create-request --delete --if-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic user-update-request --delete --if-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic user-delete-request --delete --if-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic user-create-response --delete --if-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic user-update-response --delete --if-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic user-delete-response --delete --if-exists
 
+# Create topics
 kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic code-question-create-request-to-core-service --create --partitions 3 --replication-factor 3 --if-not-exists
 kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic code-question-create-response-from-core-service --create --partitions 3 --replication-factor 3 --if-not-exists
+
 kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-create-request --create --partitions 3 --replication-factor 3 --if-not-exists
 kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-create-response --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-update-request --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-update-response --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-delete-request --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-delete-response --partitions 3 --replication-factor 3 --if-not-exists
+
 kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic user-request --create --partitions 3 --replication-factor 3 --if-not-exists
 kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic user-response --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic user-create-request --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic user-update-request --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic user-delete-request --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic user-create-response --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic user-update-response --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic user-delete-response --create --partitions 3 --replication-factor 3 --if-not-exists
