cf35183b53b36347e77fe6ea655e86cc23b24779
ndqkhanh20 <khanhndq2002@gmail.com>
2024-05-06 02:21:14 +0700

    [BE] - feat: kafka latest

--- local/create-topics-local.sh
+++ local/create-topics-local.sh
@@ -1,47 +1,39 @@
 #local-confluent-kafka-cp-zookeeper-headless
 kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --list
 
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic code-question-update-request-to-core-service --delete --if-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic code-question-update-response-from-core-service --delete --if-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic code-question-update-request-to-core-service --delete --if-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic code-question-update-response-from-core-service --delete --if-exists
       
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-request --delete --if-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-response --delete --if-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-request --delete --if-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-response --delete --if-exists
       
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic user-request --delete --if-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic user-response --delete --if-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic core-user-request --delete --if-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic course-user-request --delete --if-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic user-response --delete --if-exists
       
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic organization-request --delete --if-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic organization-create-request --delete --if-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic organization-update-request --delete --if-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic organization-delete-request --delete --if-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic organization-create-response --delete --if-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic organization-update-response --delete --if-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic organization-delete-response --delete --if-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic organization-response --delete --if-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic core-organization-request --delete --if-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic course-organization-request --delete --if-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic organization-response --delete --if-exists
       
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic calendar-event-update-request --delete --if-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic calendar-event-update-response --delete --if-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic calendar-event-update-request --delete --if-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic calendar-event-update-response --delete --if-exists
 
 # Create topics
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic code-question-update-request-to-core-service --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic code-question-update-response-from-core-service --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic code-question-update-request-to-core-service --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic code-question-update-response-from-core-service --create --partitions 3 --replication-factor 3 --if-not-exists
 
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-request --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-response --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-request --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-response --create --partitions 3 --replication-factor 3 --if-not-exists
 
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic user-request --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic user-response --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic core-user-request --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic course-user-request --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic user-response --create --partitions 3 --replication-factor 3 --if-not-exists
 
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic organization-request --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic organization-create-request --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic organization-update-request --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic organization-delete-request --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic organization-create-response --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic organization-update-response --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic organization-delete-response --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic organization-response --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic core-organization-request --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic course-organization-request --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic organization-response --create --partitions 3 --replication-factor 3 --if-not-exists
 
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic calendar-event-update-request --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics -zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic calendar-event-update-response --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic calendar-event-update-request --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --topic calendar-event-update-response --create --partitions 3 --replication-factor 3 --if-not-exists
 
 kafka-topics --zookeeper local-confluent-kafka-cp-zookeeper-headless:2181 --list
