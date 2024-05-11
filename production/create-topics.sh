46ad6ac9334fef92b20fb8d5e6dc639cfecf8c53
ndqkhanh20 <khanhndq2002@gmail.com>
2024-05-12 00:47:23 +0700

    [BE] - feat: push docker image to docker hub and use public image pull

--- production/create-topics.sh
+++ production/create-topics.sh
@@ -19,21 +19,21 @@ kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic 
 kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic calendar-event-update-response --delete --if-exists
 
 # Create topics
-kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic code-question-update-request-to-core-service --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic code-question-update-response-from-core-service --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic code-question-update-request-to-core-service --create --partitions 2 --replication-factor 2 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic code-question-update-response-from-core-service --create --partitions 2 --replication-factor 2 --if-not-exists
 
-kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-request --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-response --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-request --create --partitions 2 --replication-factor 2 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic core-question-response --create --partitions 2 --replication-factor 2 --if-not-exists
 
-kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic core-user-request --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic course-user-request --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic user-response --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic core-user-request --create --partitions 2 --replication-factor 2 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic course-user-request --create --partitions 2 --replication-factor 2 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic user-response --create --partitions 2 --replication-factor 2 --if-not-exists
 
-kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic core-organization-request --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic course-organization-request --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic organization-response --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic core-organization-request --create --partitions 2 --replication-factor 2 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic course-organization-request --create --partitions 2 --replication-factor 2 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic organization-response --create --partitions 2 --replication-factor 2 --if-not-exists
 
-kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic calendar-event-update-request --create --partitions 3 --replication-factor 3 --if-not-exists
-kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic calendar-event-update-response --create --partitions 3 --replication-factor 3 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic calendar-event-update-request --create --partitions 2 --replication-factor 2 --if-not-exists
+kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --topic calendar-event-update-response --create --partitions 2 --replication-factor 2 --if-not-exists
 
 kafka-topics --zookeeper gke-confluent-kafka-cp-zookeeper-headless:2181 --list
